4d9dd2571436be61628bcaf0b5ef59ed5bab3f40
Jaisal <Mohammed_JaisalThumbithodika@comcast.com>
2025-10-03 10:18:35 +0530

    aasas

--- LearningAWS/urls.py
+++ LearningAWS/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,3 +25,7 @@ urlpatterns = [
     # DRF auth URLs for browsable API login/logout
     path('api-auth/', include('rest_framework.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
